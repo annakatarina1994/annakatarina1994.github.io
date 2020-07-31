e568bcde1c3a737b14bdfdd2e3abbb43f3a26624
Anna Lizon <annakatarina1994@gmail.com>
2020-07-31 15:33:28 -0400

    new portfolio beginnings

---
+++ main.js
@@ -0,0 +1,43 @@
+const menuBtn = document.querySelector('.menu-btn');
+const contactBtn = document.querySelector('.contact');
+const techStackBtn = document.querySelector('.techStack');
+const projectsBtn = document.querySelector('.projects');
+const aboutBtn = document.querySelector('.aboutMe');
+
+let menuOpen = false;
+
+menuBtn.addEventListener('click', ()=>{
+  if(!menuOpen){
+    menuBtn.classList.add('open');
+
+    contactBtn.classList.remove('close');
+    contactBtn.classList.add('open');
+
+    techStackBtn.classList.remove('close');
+    techStackBtn.classList.add('open');
+
+    projectsBtn.classList.remove('close');
+    projectsBtn.classList.add('open');
+
+    aboutBtn.classList.remove('close');
+    aboutBtn.classList.add('open');
+
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+
+    contactBtn.classList.remove('open');
+    contactBtn.classList.add('close');
+
+    techStackBtn.classList.remove('open');
+    techStackBtn.classList.add('close');
+
+    projectsBtn.classList.remove('open');
+    projectsBtn.classList.add('close');
+
+    aboutBtn.classList.remove('open');
+    aboutBtn.classList.add('close');
+    
+    menuOpen = false;
+  }
+});
